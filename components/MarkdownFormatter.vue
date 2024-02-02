1290e2c0a6f27353100ea9840bb8e74ad8107888
Andrei <andrei.hudalla@outlook.com>
2024-02-02 13:13:33 +0300

    Update MarkdownFormatter.vue

--- components/MarkdownFormatter.vue
+++ components/MarkdownFormatter.vue
@@ -11,6 +11,20 @@
 	h1 {
 		font-weight: 700;
 		font-size: 2rem;
+		margin: 1rem 0;
+	}
+	h2 {
+		font-weight: 500;
+		font-size: 1.5rem;
+		margin: 0.75rem 0;
+	}
+	h3 {
+		font-weight: 300;
+		font-size: 1.2rem;
+		margin: 0.5rem 0;
+	}
+	p {
+		opacity: 0.8;
 	}
 }
 </style>
